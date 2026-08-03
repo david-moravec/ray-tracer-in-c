b0907c1256de8699fd557aff7727d08bcb95adad
David Moravec <moravec@numsolution.cz>
2026-08-03 15:07:24 +0200

    fix: color of surfaces did not respect the surface normal

--- hittable.h
+++ hittable.h
@@ -115,7 +115,13 @@ static inline bool hittable_list_hit(HittableList *list, Ray ray,
     if (hittable_hit(hittable, ray, ray_tmin, ray_tmax, record)) {
       anything_hitted = true;
       current_closest = temp_record.t;
-      *record = temp_record;
+      // STUDY why the commented line is wrong, i thooght
+      // it will overwrite the fields of recrod with contenst
+      // of temp_record. Which should be equvivalent to record now
+      // pointing to temp record. At the end record and temp_record
+      // should have the same value in fields?
+      // *record = temp_record;
+      record = &temp_record;
     }
   }
 
