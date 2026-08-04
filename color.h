b071b6cbcc16449718eb2ee366f21eab9cfc1d43
David Moravec <moravec@numsolution.cz>
2026-08-04 13:23:23 +0200

    refactor: use interaval_clamp

--- color.h
+++ color.h
@@ -1,8 +1,11 @@
 #ifndef RAYTRACER_COLOR_H
 #define RAYTRACER_COLOR_H
 
+#include "interval.h"
 #include "stdio.h"
 #include "vec3.h"
+
+static const Interval _INTENSITY = (Interval){.min = 0.0, .max = 0.999};
 
 typedef Vec3 Color;
 
@@ -15,9 +18,9 @@ static inline void color_fprint(FILE *f, const Color *color) {
   double g = color->y;
   double b = color->z;
 
-  int rbyte = (int)(255.999 * r);
-  int gbyte = (int)(255.999 * g);
-  int bbyte = (int)(255.999 * b);
+  int rbyte = (int)(256 * interval_clamp_value(_INTENSITY, r));
+  int gbyte = (int)(256 * interval_clamp_value(_INTENSITY, g));
+  int bbyte = (int)(256 * interval_clamp_value(_INTENSITY, b));
 
   fprintf(f, "%d %d %d\n", rbyte, gbyte, bbyte);
 }
