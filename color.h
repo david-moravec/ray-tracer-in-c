b8ce7745d56e707a50fe6c966ac1148e3176dcc5
David Moravec <moravec@numsolution.cz>
2026-08-05 07:42:22 +0200

    feat: add gamma 2

--- color.h
+++ color.h
@@ -13,10 +13,22 @@ static inline Color color_new(double x, double y, double z) {
   return vec3_new(x, y, z);
 }
 
+static inline double linear_to_gamma(double linear_component) {
+  if (linear_component > 0) {
+    return sqrt(linear_component);
+  }
+
+  return 0.0;
+}
+
 static inline void color_fprint(FILE *f, const Color color) {
   double r = color.x;
   double g = color.y;
   double b = color.z;
+
+  r = linear_to_gamma(r);
+  g = linear_to_gamma(g);
+  b = linear_to_gamma(b);
 
   int rbyte = (int)(256 * interval_clamp_value(_INTENSITY, r));
   int gbyte = (int)(256 * interval_clamp_value(_INTENSITY, g));
