44b12f775f5974beddf8efb1dc7c64dddab854f0
David Moravec <moravec@numsolution.cz>
2026-08-06 15:35:36 +0200

    final scene render

--- main.c
+++ main.c
@@ -1,3 +1,4 @@
+#include "rtcommon.h"
 #include "stdbool.h"
 #include "stdint.h"
 #include "time.h"
@@ -12,43 +13,69 @@ int main() {
   Arena *arena = arena_alloc(1 << 20, 0);
   Hittable world = hittable_collection_new(arena);
 
-  double R = cos(M_PI_4);
-
-  Material material_ground = material_new_lambertian(color_new(0.8, 0.8, 0.0));
-  Material material_center = material_new_lambertian(color_new(0.1, 0.2, 0.5));
-  Material material_left = material_new_dielectrics(1.50);
-  Material material_bubble = material_new_dielectrics(1.00 / 1.50);
-  Material material_right = material_new_metal(color_new(0.8, 0.6, 0.2), 1.0);
-
+  Material material_ground = material_new_lambertian(color_new(0.5, 0.5, 0.5));
   hittable_list_add(world.collection,
-                    hittable_sphere_new(point3_new(0.0, -100.5, -1.0), 100,
+                    hittable_sphere_new(point3_new(0.0, -1000.0, 0.0), 1000.0,
                                         &material_ground));
+
+  for (int a = -11; a < 11; a++) {
+    for (int b = -11; b < 11; b++) {
+      double choose_mat = random_double();
+      Point3 center =
+          point3_new(a + 0.9 * random_double(), 0.2, b + 0.9 * random_double());
+
+      if ((vec3_length(vec3_subtract(center, point3_new(4, 0.2, 0)))) > 0.9) {
+        Material *sphere_material = arena_push(arena, sizeof(Material));
+
+        if (choose_mat < 0.8) {
+          Color albedo = vec3_multiply(vec3_random(), vec3_random());
+          sphere_material->type = RAYTRACER_MATERIAL_LAMBERTIAN;
+          sphere_material->albedo = albedo;
+        } else if (choose_mat < 0.95) {
+          Color albedo = vec3_random_in_interval(0.5, 1.0);
+          double fuzz = random_double_in_interval(0.0, 0.5);
+          sphere_material->type = RAYTRACER_MATERIAL_METAL;
+          sphere_material->albedo = albedo;
+          sphere_material->fuzz = fuzz;
+        } else {
+          sphere_material->type = RAYTRACER_MATERIAL_DIELECTRICS;
+          sphere_material->refraction_index = 1.5;
+        }
+
+        hittable_list_add(world.collection,
+                          hittable_sphere_new(center, 0.2, sphere_material));
+      }
+    }
+  }
+
+  Material material_1 = material_new_dielectrics(1.50);
   hittable_list_add(
       world.collection,
-      hittable_sphere_new(point3_new(0.0, 0.0, -1.2), 0.5, &material_center));
+      hittable_sphere_new(point3_new(0.0, 1.0, 0), 1, &material_1));
+
+  Material material_2 = material_new_lambertian(color_new(0.4, 0.2, 0.1));
   hittable_list_add(
       world.collection,
-      hittable_sphere_new(point3_new(-1.0, 0, -1.0), 0.5, &material_left));
+      hittable_sphere_new(point3_new(-4.0, 1, 0.0), 1.0, &material_2));
+
+  Material material_3 = material_new_metal(color_new(0.7, 0.6, 0.5), 0.0);
   hittable_list_add(
       world.collection,
-      hittable_sphere_new(point3_new(-1.0, 0, -1.0), 0.4, &material_bubble));
-  hittable_list_add(
-      world.collection,
-      hittable_sphere_new(point3_new(1.0, 0, -1.0), 0.5, &material_right));
+      hittable_sphere_new(point3_new(4.0, 1.0, 0.0), 1.0, &material_3));
 
   Camera camera = {0};
   camera.aspect_ratio = 16.0 / 9.0;
-  camera.image_width = 400;
-  camera.samples_per_pixel = 100;
+  camera.image_width = 1200;
+  camera.samples_per_pixel = 500;
   camera.max_depth = 50;
 
   camera.vfov = 20;
-  camera.look_from = point3_new(-2.0, 2.0, 1.0);
-  camera.look_at = point3_new(0.0, 0.0, -1.0);
+  camera.look_from = point3_new(13.0, 2, 3);
+  camera.look_at = point3_new(0.0, 0.0, 0);
   camera.vup = vec3_new(0.0, 1.0, 0.0);
 
-  camera.defocus_angle = 10.0;
-  camera.focus_dist = 3.4;
+  camera.defocus_angle = 0.6;
+  camera.focus_dist = 10.0;
 
   camera_initialize(&camera);
   double start_time = (double)clock() / CLOCKS_PER_SEC;
